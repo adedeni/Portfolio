b2224acaf34b01a3dc908781bb3f9af443af51d0
adedeni <adeshinaolayodemubaraq@gmail.com>
2025-09-06 20:10:50 +0100

    worked on projects carousel

--- index.php
+++ index.php
@@ -743,46 +743,46 @@
         <div class="container-fluid p-0">
             <div class="row g-0">
                 <div class="col-lg-4 col-sm-6">
-                    <a class="portfolio-box" href="assets/img/portfolio/fullsize/1.jpg" title="Project Name">
-                        <img class="img-fluid" src="assets/img/portfolio/thumbnails/1.jpg" alt="..." />
+                    <a class="portfolio-box" href="https://mobilenig.com" title="Mobilenig Website">
+                        <img class="img-fluid" src="assets\img\portfolio\thumbnails\mobilenig.png" alt="..." />
                         <div class="portfolio-box-caption">
-                            <div class="project-category text-white-50">Category</div>
-                            <div class="project-name">Project Name</div>
+                            <div class="project-category text-white-50">Websites</div>
+                            <div class="project-name">Mobilenig Website</div>
                         </div>
                     </a>
                 </div>
                 <div class="col-lg-4 col-sm-6">
-                    <a class="portfolio-box" href="assets/img/portfolio/fullsize/2.jpg" title="Project Name">
-                        <img class="img-fluid" src="assets/img/portfolio/thumbnails/2.jpg" alt="..." />
+                    <a class="portfolio-box" href="https://naira4coin.com" title="Project Name">
+                        <img class="img-fluid" src="assets\img\portfolio\thumbnails\naira4coin.png" alt="..." />
                         <div class="portfolio-box-caption">
-                            <div class="project-category text-white-50">Category</div>
-                            <div class="project-name">Project Name</div>
+                            <div class="project-category text-white-50">Websites</div>
+                            <div class="project-name">Naira4coin Website</div>
                         </div>
                     </a>
                 </div>
                 <div class="col-lg-4 col-sm-6">
-                    <a class="portfolio-box" href="assets/img/portfolio/fullsize/3.jpg" title="Project Name">
-                        <img class="img-fluid" src="assets/img/portfolio/thumbnails/3.jpg" alt="..." />
+                    <a class="portfolio-box" href="https://easil.com.ng" title="Project Name">
+                        <img class="img-fluid" src="assets\img\portfolio\thumbnails\easil.png" alt="..." />
                         <div class="portfolio-box-caption">
-                            <div class="project-category text-white-50">Category</div>
-                            <div class="project-name">Project Name</div>
+                            <div class="project-category text-white-50">Websites</div>
+                            <div class="project-name">Easil Website</div>
                         </div>
                     </a>
                 </div>
                 <div class="col-lg-4 col-sm-6">
-                    <a class="portfolio-box" href="assets/img/portfolio/fullsize/4.jpg" title="Project Name">
-                        <img class="img-fluid" src="assets/img/portfolio/thumbnails/4.jpg" alt="..." />
+                    <a class="portfolio-box" href="https://easil.com.ng/apps/swappay" title="Project Name">
+                        <img class="img-fluid" src="assets\img\portfolio\thumbnails\swappay.png" alt="..." />
                         <div class="portfolio-box-caption">
-                            <div class="project-category text-white-50">Category</div>
-                            <div class="project-name">Project Name</div>
+                            <div class="project-category text-white-50">Mobile App</div>
+                            <div class="project-name">SWapPay App</div>
                         </div>
                     </a>
                 </div>
                 <div class="col-lg-4 col-sm-6">
-                    <a class="portfolio-box" href="assets/img/portfolio/fullsize/5.jpg" title="Project Name">
-                        <img class="img-fluid" src="assets/img/portfolio/thumbnails/5.jpg" alt="..." />
+                    <a class="portfolio-box" href="https://easil.com.ng/apps/ushopia" title="Project Name">
+                        <img class="img-fluid" src="assets\img\portfolio\thumbnails\ushopia.png" alt="..." />
                         <div class="portfolio-box-caption">
-                            <div class="project-category text-white-50">Category</div>
+                            <div class="project-category text-white-50">Mobile App</div>
                             <div class="project-name">Project Name</div>
                         </div>
                     </a>
